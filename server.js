846361d17527e1806fdd82fe169fe2ede33cf9f6
Sam Hamm <samhamm@gmail.com>
2017-02-20 10:46:42 -0800

    couple of little edits

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 const express = require('express')
 const app = express()
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000
 
 app.use(express.static('./public'))
 
-function someFunk(req, res) {
-  res.sendFile('index.html', {root: './public'})
-}
+app.get('*', (req, res) => {
+  res.sendFile('index.html', { root: './public' })
+})
 
-app.get('/', someFunk)
-
-app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
